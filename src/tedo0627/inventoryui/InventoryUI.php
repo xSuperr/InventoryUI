c0c6834e54ac09a2409bd527161e11192f8948ae
xSuper <81652889+xSuperr@users.noreply.github.com>
2026-01-08 20:35:53 -0500

    Update InventoryUI.php

--- src/tedo0627/inventoryui/InventoryUI.php
+++ src/tedo0627/inventoryui/InventoryUI.php
@@ -11,11 +11,15 @@ class InventoryUI {
 
     private static bool $setup = false;
 
+    private static PluginBase $instance;
+
     private const uuid = "21f0427f-572a-416d-a90e-c5d9becb0fa3";
     private const version = "1.1.0";
 
     public static function setup(PluginBase $plugin): void {
         if (self::$setup) return;
+
+        self::$instance = $plugin;
 
         $server = $plugin->getServer();
 
@@ -28,17 +32,32 @@ class InventoryUI {
         $plugin->getScheduler()->scheduleRepeatingTask(new InventoryTickTask($server), 1);
 
         $nbt = new CompoundTag();
-        $nbt->setString("bid", "");
+        $nbt->setString("bid", "minecraft:");
         $nbt->setByte("hasspawnegg", false);
         $nbt->setString("id", "inventoryui:inventoryui");
-        $nbt->setByte("summonable", false);
+        $nbt->setByte("summonable", true);
 
+        $maxRuntimeId = -1;
         $packet = StaticPacketCache::getInstance()->getAvailableActorIdentifiers();
         $tag = $packet->identifiers->getRoot();
         if ($tag instanceof CompoundTag) {
-            $tag->getListTag("idlist")->push($nbt);
+            $list = $tag->getListTag("idlist");
+            if ($list !== null) {
+                foreach ($list->getValue() as $childTag) {
+                    if (!($childTag instanceof CompoundTag)) continue;
+
+                    $maxRuntimeId = max($childTag->getInt("rid"), $maxRuntimeId);
+                }
+            }
+
+            $nbt->setInt("rid", $maxRuntimeId + 1);
+            $list->push($nbt);
         }
 
         self::$setup = true;
+    }
+
+    public static function getInstance(): PluginBase {
+        return self::$instance;
     }
 }
